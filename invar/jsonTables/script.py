b50763d0d6b7bacf90fdb01fda73b93df7e094bd
Sophia Kramar <krasofija@yandex.ru>
2018-12-05 22:12:06 +0300

    Create script.py

---
+++ invar/jsonTables/script.py
@@ -0,0 +1,28 @@
+import json
+from tabulate import tabulate
+import re
+
+try:
+    f = open("strings.json", 'rb')
+except IOError:
+    print("Could not read file: strings.json")
+    sys.exit()
+
+with open("strings.json") as json_data:
+    d = json.load(json_data)
+    #print(type(d))
+with open("newRaw.txt", 'w') as file:
+    file.write(tabulate(d))
+    file.close()
+
+try:
+    f = open("newRaw.txt", 'rb')
+except IOError:
+    print("Could not read file: newRaw.txt")
+    sys.exit()
+
+with open("newRaw.txt") as open_file:
+    data = open_file.read()
+
+with open("newTable.txt", 'w') as file:
+    file.write(re.sub('[{}:,\']', '', data))
